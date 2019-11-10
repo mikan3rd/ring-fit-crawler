0ac9affe4dbd46c0ccb4ccbe434a78eb584d26e2
mikan3rd <mikan.no.kikoushi@gmail.com>
2019-11-10 23:51:06 +0900

    perf: puppeteerの追加

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,14 +1,28 @@
 import * as functions from 'firebase-functions';
-import puppeteer from 'puppeteer';
+import * as puppeteer from 'puppeteer';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send('Hello from Firebase!');
-});
-
-export const helloPubSub = functions.pubsub.topic('ring-fit-adventure').onPublish(message => {
+export const helloPubSub = functions.pubsub.topic('ring-fit-adventure').onPublish(async message => {
+  console.log('START');
   console.log(message);
+
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
+  const page = await browser.newPage();
+
+  const url = 'https://store.nintendo.co.jp/item/HAC_Q_AL3PA.html';
+  await page.goto(url);
+
+  await page.$eval('.item-cart-add-area__add-button', item => {
+    console.log('BUTTON');
+    console.log(item.textContent);
+  });
+
+  await browser.close();
+  console.log('FINISH');
 });
