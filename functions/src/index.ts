88c0595667c6e3f039a39be82d0f452b6a786514
mikan3rd <mikan.no.kikoushi@gmail.com>
2019-11-11 00:45:33 +0900

    feat: getRingFitSaleStatus関数に処理を切り出し

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,12 +3,18 @@ import * as puppeteer from 'puppeteer';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
-//
+
+export const helloWorld = functions.https.onRequest(async (request, response) => {
+  const result = await getRingFitSaleStatus();
+  response.json({ result });
+});
 
 export const helloPubSub = functions.pubsub.topic('ring-fit-adventure').onPublish(async message => {
-  console.log('START');
-  console.log(message);
+  const result = await getRingFitSaleStatus();
+  return result;
+});
 
+const getRingFitSaleStatus = async () => {
   const browser = await puppeteer.launch({
     headless: true,
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
@@ -18,11 +24,11 @@ export const helloPubSub = functions.pubsub.topic('ring-fit-adventure').onPublis
   const url = 'https://store.nintendo.co.jp/item/HAC_Q_AL3PA.html';
   await page.goto(url);
 
-  await page.$eval('.item-cart-add-area__add-button', item => {
-    console.log('BUTTON');
-    console.log(item.textContent);
-  });
+  const content = await page.$eval('.item-cart-add-area__add-button', item => item.textContent);
+
+  console.log('content', content);
 
   await browser.close();
-  console.log('FINISH');
-});
+
+  return content;
+};
